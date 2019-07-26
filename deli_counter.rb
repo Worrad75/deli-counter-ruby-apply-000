102b7f924b9a4305141800fc711a156e6a8aaec2
Darrow Palast <dpalast@gmail.com>
2019-07-26 18:10:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,49 +1,3 @@
-#CURVE BALL (?)
-
-# def take_a_number(line_arr, name)
-
-#     line_arr << name
-#     puts "welcome, #{name}, you are number #{line_arr.count} in line"
-
-# end
-
-# def take_a_number(line_counter)
-
-#     line_counter += 1
-#     puts "Welcome, you are number #{line_counter} in line"
-
-# end
-
-
-
-# SQUARE ARRAY LAB
-
-# def square_array(numbers)
-#   numbers.collect { |number| number**2 }
-# end
-
-# nums = [1,2,3,4,5]
-# p square_array(nums)
-
-# -----
-
-# def line(array)
-#   line_array = []
-  
-#   array.each.with_index do |name, index|
-#     line_array.push("#{index}: #{name}, ")
-#   end
-#   puts "the line is currently: #{line_array.join("")}"
-  
-# end
-
-# people = ["sam", "bobby", "maia"]
-# puts line(people)
-
-
-
-# DELI COUNTER LAB
-
 def line(array)
   line_array = []
   
@@ -59,7 +13,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome to Katz Deli, #{name}! You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
