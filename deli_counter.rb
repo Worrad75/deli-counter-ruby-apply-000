48eb63596d060eb7c8bf71f86fecffec95bc1431
Darrow Palast <dpalast@gmail.com>
2019-07-26 17:46:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def line(array)
-  line_array = [ ]
+  line_array = []
   puts "the line is currently: "
   array.each.with_index do |name, index|
       print "#{index}: #{name}. "
