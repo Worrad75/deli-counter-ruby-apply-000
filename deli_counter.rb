37af6a0ee329162321dd0f9234ea21c5e08da132
Darrow Palast <dpalast@gmail.com>
2019-07-26 18:01:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome to Katz Deli, #{name}! You are number #{katz_deli.length} in line."
 end
 
-def now_serving
-  
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts 
   
 end
