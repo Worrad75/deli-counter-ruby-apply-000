8ae58e249e5f2d9e214b1ac8a69e803de59f0d54
Darrow Palast <dpalast@gmail.com>
2019-07-26 17:47:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,12 @@
 
 def line(array)
   line_array = []
-  puts "the line is currently: "
+  
   array.each.with_index do |name, index|
     line_array.push("#{index}: #{name}. ")
   end
+  
+  puts "the line is currently: "
   
 end
 
